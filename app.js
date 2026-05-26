025d067129d52393135cc369c157b08a5d59ee7a
Utkarsh Mishra <utkarsh2004mishra@gmail.com>
2026-05-26 23:19:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const dbUrl =process.env.DB_URL
 //'mongodb://localhost:27017/yelp-camp'
-
+app.set('trust proxy', 1);
 mongoose.connect( dbUrl)
     .then(() => console.log('Database connected'))
     .catch(err => console.log(err));
@@ -154,6 +154,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
