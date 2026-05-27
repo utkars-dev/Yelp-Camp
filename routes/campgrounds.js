237b1a8bac2a375c1aa6d0da498ac116a1ff0f9f
Utkarsh Mishra <utkarsh2004mishra@gmail.com>
2026-05-28 01:14:55 +0530

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,5 +1,83 @@
 const express = require('express');
 const router = express.Router();
+
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+
+const { 
+    isLoggedIn, 
+    isAdmin,
+    validateCampground 
+} = require('../middleware');
+
+const multer = require('multer');
+
+const { storage } = require('../cloudinary');
+
+const upload = multer({ storage });
+
+const Campground = require('../models/campground');
+
+
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+
+    .post(
+        isLoggedIn,
+        isAdmin,
+        upload.array('image'),
+        validateCampground,
+        catchAsync(campgrounds.createCampground)
+    );
+
+
+
+router.get(
+    '/new',
+    isLoggedIn,
+    isAdmin,
+    campgrounds.renderNewForm
+);
+
+
+
+router.route('/:id')
+
+    .get(catchAsync(campgrounds.showCampground))
+
+    .put(
+        isLoggedIn,
+        isAdmin,
+        upload.array('image'),
+        validateCampground,
+        catchAsync(campgrounds.updateCampground)
+    )
+
+    .delete(
+        isLoggedIn,
+        isAdmin,
+        catchAsync(campgrounds.deleteCampground)
+    );
+
+
+
+router.get(
+    '/:id/edit',
+    isLoggedIn,
+    isAdmin,
+    catchAsync(campgrounds.renderEditForm)
+);
+
+
+
+module.exports = router;
+
+
+
+
+/*const express = require('express');
+const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
@@ -25,4 +103,4 @@ router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditF
 
 
 
-module.exports = router;
+module.exports = router;*/
