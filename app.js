adcf1587f8f379fd7ae113274d0f02c1da4721fb
Utkarsh Mishra <utkarsh2004mishra@gmail.com>
2026-05-28 00:38:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded({ extended: true }));
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
-//const dbUrl =process.env.DB_URL
+//const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl =process.env.DB_URL
 //'mongodb://localhost:27017/yelp-camp'
 app.set('trust proxy', 1);
 mongoose.connect( dbUrl)
